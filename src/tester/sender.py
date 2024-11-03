8d768ba3bffcc0409c7c6824b7de24bdaa348fb0
laurenzooz <lauri.pahkala@hotmail.com>
2024-11-03 14:19:50 +0200

    update tester

--- src/tester/sender.py
+++ src/tester/sender.py
@@ -2,12 +2,17 @@ import serial
 import time
 
 # Open the serial port. Replace 'COM3' with your ESP32's port and set the correct baud rate.
-ser = serial.Serial('/dev/ttyACM3', 115200)
-time.sleep(2)  # Give some time to establish connection
+ser = serial.Serial('/dev/ttyACM4', 115200)
+time.sleep(15)  # Give some time to establish connection
 
 # Open the text file and send each line over serial
-while (True):
-	ser.write(1200)  # Send value with newline
-	time.sleep(0.05)
+with open('data.txt', 'r') as file:
+    for line in file:
+        value = line.strip()  # Get the data value and remove any whitespace
+        if value:  # Make sure the line isn't empty
+            ser.write((value + '\n').encode())  # Send value with newline
+            time.sleep(0.01)  # Small delay to prevent buffer overflow
+        else:
+            print("Skipped an empty line.")
 
 ser.close()
